3995f53b1f7e7c001cca50fe70e70288c16d8304
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 05:16:29 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+def square_Array(numbers)
+  numbers = [1,2,3]
+  Array.each { |i| array << i ** 2 }
+  square_Array([1,2,3])
 end
+
+def square_array(array)
+  array.each_with_object([]) { |i,arr| arr << i ** 2 }
+end
+
+my_arr = [1, 2]
+p square_array(my_arr) #=> [1, 4]
